e9e3b6b81efb9f57d79b6403b3ccd873225be9b3
binetouguey3s <binetouguey3s@gmail.com>
2026-04-04 17:20:22 +0000

    Creation des modèless et personnalisation de l' interface admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Categorie, Article, Commentaire
 
-# Register your models here.
+
+@admin.register(Categorie)
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ('nomCategorie', 'description', 'dateCreation', 'createur')
+    search_fields = ('nomCategorie', 'description')
+    list_filter = ('dateCreation',)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'date_publication', 'publier', 'categorie', 'auteur')
+    search_fields = ('titre', 'contenu')
+    list_filter = ('date_publication', 'publier', 'categorie')
+
+@admin.register(Commentaire)
+class CommentaireAdmin(admin.ModelAdmin):
+    list_display = ('article', 'dateCommentaire', 'username')
+    search_fields = ('message',)
+    list_filter = ('dateCommentaire',)
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Categorie(models.Model):
+    nomCategorie = models.CharField(max_length=255)
+    description = models.TextField()
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    createur = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.nomCategorie
+ 
+
+
+class Article(models.Model):
+    titre = models.CharField(max_length=255)
+    contenu = models.TextField()
+    date_publication = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='articles/')
+    publier = models.BooleanField(default=True)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    auteur = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+    def __str__(self):
+        return self.titre
+
+
+class Commentaire(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    dateCommentaire = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()    
+    username = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+    def __str__(self):
+        return f"Commentaire sur {self.username} à {self.dateCommentaire}"
+    
